726b5dea3572c8c9aac31f810d1b51b676370115
Eberson Oliveira <eberson.oliveira@gmail.com>
2018-08-20 20:09:57 -0300

    Inclusao de Arquivos para Auxilio dos Alunos

---
+++ init.sh
@@ -0,0 +1,5 @@
+git clone git remote add origin https://github.com/eberson/tpi-ii.git
+git config --global user.email "eberson.oliveira@gmail.com"
+git config --global user.name "Eberson Oliveira"
+cd tpi-ii
+code .
