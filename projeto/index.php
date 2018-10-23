fed0718c870bd10616ea3027b95979c1bbf87035
Eberson Oliveira <eberson.oliveira@gmail.com>
2018-10-22 22:43:02 -0300

    aula 22/10

---
+++ projeto/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <title>Projeto Demo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
+    <link href="css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="login-container">
+            <div id="output"></div>
+            <div class="avatar"></div>
+            <div class="form-box">
+                <form action="" method="">
+                    <input name="user" type="text" placeholder="usuário">
+                    <input type="password" placeholder="senha">
+                    <button class="btn btn-info btn-block login" type="submit">Entrar</button>
+                </form>
+            </div>
+        </div>        
+    </div>
+
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/login.js"></script>
+</body>
+</html>
